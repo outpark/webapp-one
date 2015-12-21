2ef3b70d9de35e69d7e61ceb3c2507e41b678b87
Saj Arora <saj.arora24@gmail.com>
2015-12-20 20:44:19 -0500

    Added login page

---
+++ main/public/modules/mainapp/auth/auth.controller.js
@@ -0,0 +1,14 @@
+//DashboardController
+//Controls all the dasbhoard stuff
+(function(){
+    'use strict';
+    var module = angular.module('mainapp');
+
+    module.controller('LoginController', function(){
+
+    });
+
+    module.controller('SignupController', function(){
+
+    });
+}());
--- main/public/modules/mainapp/config/app.routes.js
+++ main/public/modules/mainapp/config/app.routes.js
@@ -14,12 +14,17 @@
                 abstract: true,
                 templateUrl:'/p/modules/mainapp/layout/app.view.html'
             })
-            .state('app.signin', {
-                url         : '/signin',
+            .state('signup', {
+                url         : '/app/signup',
+                controller  : 'SignupController',
+                templateUrl : '/p/modules/mainapp/auth/signup.view.html'
+            })
+            .state('login', {
+                url         : '/app/login',
                 controller  : 'LoginController',
                 templateUrl : '/p/modules/mainapp/auth/login.view.html'
             })
-            .state('app.logout', {
+            .state('logout', {
                 url         : '/logout',
                 controller: function (Restangular, gaAuthentication, $state, gaAppConfig) {
                     Restangular.all('auth/signout').post().then(function (appConfig) {
